34fd3995a41412cf0f778490d3e7eede59955da0
Will Amlong <amlongwi@gmail.com>
2026-04-29 15:31:08 -0500

    added db connection and set up db on mongo

---
+++ server/models/Transaction.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["income", "expense"],
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
